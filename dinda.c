0c8e2e0d51955f3cef384d4d259fdfc82e2802ea
Wildan Angga Rahman <wildan.aga@gmail.com>
2014-04-25 15:03:30 +0700

    Update dinda.c

--- dinda.c
+++ dinda.c
@@ -1,99 +1,153 @@
 #include <stdio.h>
 #include <conio.h>
 
-main ()//main program
+int main(void)
 {
-//judul
-       printf ("Test Your Brain Now\n");
-       
-       int a,b,c,score;
-       score = 0;
-       printf("Hitung kemampuan berhitungmu: ");
-       scanf("%d",&a);
-       printf("Hitung kemampuanmu berhitungmu: ");
-       scanf("%d",&b);
-       c=a+b;
-       printf("? %d + %d = %d",a,b,c);
-       
-       printf("4 + 4 = ");
-       scanf("%d",&c);
-        
-          if(c = 8)
-          {
-               printf("\nJawaban anda benar\n");
-               score = score + 25;
-               printf("Score anda %d",&score);
-               printf("\nKetik sembarang untuk melanjutkan\n");
-               getch();
-               
-               if(score = 25)
-               {
-                   printf("18-12 = ");
-                   scanf("%d",&c);
-                   
-                   if(c = 6)
-                   {
-                      printf("\nJawaban anda benar\n");
-                      score = score + 25;
-                      printf("Score anda %d",&score);
-                      getch()  
-                      
-                      if(score = 50)
-                      {
-                          printf("2 x 7 = ");
-                          scanf("%d",&c);
-                          
-                          if(c= 14)
-                          {
-                            printf("\nJawaban anda benar\n");
-                            score = score + 25;
-                            printf("Score anda %d",&score);
-                            getch();
-                            
-                            if(score = 75)
-                            {
-                             printf("100 / 2 = ");
-                             scanf("%d",&c);
-                             
-                             if(c = 50)
-                             {
-                                printf("\nJawaban anda benar\n");
-                                score = score + 25;
-                                printf("Score anda %d",&score);
-                                Printf("Selesai");
-                                getch();
-                                break;
-                                       
-                             }
-                            
-                            }else
-                            {
-                                 break;
-                            }
-                            
-                            
-                            
-                          }       
-                      }else
-                      {
-                         break; 
-                      } 
-                      
-                      
-                   }else
-                   {
-                        break;
-                   }
-               }    
-          }else
-           {
-            printf("Jawaban anda salah\n");
-            printf("Score anda %d",&score);   
-           }         
-       
-       
-getch();
-return 0;
-}
+	int menu, answer, score, jawaban,nilai1,nilai2;
+	
 
+
+	printf("Test Your Brain Now\n");
+	getch();
+	do
+	{
+		system("cls");
+		printf("1. Play\n");
+		printf("2. Quit\n");
+		printf("Choice your menu : ");
+		scanf("%d", &menu);
+
+		switch (menu)
+		{
+		case 1:
+			score = 0;
+			system("cls");
+			printf("1. masukan nilai-nilai penjumlahan yang menghasilkan 175 ? \n\n");
+			printf("Jawaban : \n");
+			printf("Nilai 1:");
+			scanf("%d", &nilai1);
+			printf("Nilai 2:");
+			scanf("%d", &nilai2);
+	
+			answer = nilai1 + nilai2;
+				
+			if (answer == 175)
+			{
+				printf("Benar\n");
+				jawaban = 1;
+				score = score + 25;
+				printf("Score mu : %d", score);
+				printf("\nTekan sembarang untuk melanjutkan");
+				getch();
+				
+				if (score == 25)
+				{
+					system("cls");
+					printf("2. masukan nilai-nilai pengurangan yang menghasilkan 54? \n\n");
+					printf("Jawaban : \n");
+					printf("Nilai 1:");
+					scanf("%d", &nilai1);
+					printf("Nilai 2:");
+					scanf("%d", &nilai2);
+
+					answer = nilai1 - nilai2;
+					if (answer == 54)
+					{
+						printf("Benar\n");
+						jawaban = 1;
+						score = score + 25;
+						printf("Score mu : %d", score);
+						printf("\nTekan sembarang untuk melanjutkan");
+						getch();
+
+						if (score == 50)
+						{
+							system("cls");
+							printf("3. masukan nilai-nilai perkalian yang menghasilkan 90? \n\n");
+							printf("Jawaban : \n");
+							printf("Nilai 1:");
+							scanf("%d", &nilai1);
+							printf("Nilai 2:");
+							scanf("%d", &nilai2);
+
+							answer = nilai1 * nilai2;
+						}
+
+						if (answer == 90)
+						{
+							printf("Benar\n");
+							jawaban = 1;
+							score = score + 25;
+							printf("Score mu : %d", score);
+							printf("\nTekan sembarang untuk melanjutkan");
+							getch();
+
+							if (score == 75)
+							{
+								system("cls");
+								printf("4.  masukan nilai-nilai pengurangan yang menghasilkan 2? \n\n");
+								printf("Jawaban : \n");
+								printf("Nilai 1:");
+								scanf("%d", &nilai1);
+								printf("Nilai 2:");
+								scanf("%d", &nilai2);
+
+								answer = nilai1 / nilai2;;
+
+								if (answer == 2)
+								{
+									printf("Benar\n");
+									jawaban = 1;
+									score = score + 25;
+									printf("Score mu : %d", score);
+									printf("\nTekan sembarang untuk melanjutkan");
+									getch();
+									break;
+								}
+								else
+								{
+									printf("Ooops, Jawaban salah");
+									jawaban = 0;
+								}
+							}
+
+						}
+						else
+						{
+							printf("Ooops, Jawaban salah");
+							jawaban = 0;
+						}
+
+
+					}
+					else
+					{
+						printf("Ooops, Jawaban salah");
+						jawaban = 0;
+					}
+
+				}
+			}
+			else
+			{
+				printf("Ooops, Jawaban salah");
+				jawaban = 0;
+			}
+
+			getch();
+			break;
+		case 2:
+			system("cls");
+			printf("bye");
+			break;
+		default:
+			printf("wrong input");
+			getch();
+			break;
+		}
+
+	} while (menu != 2 || jawaban != 0);
+	getch();
+	return 0;
+}
 
